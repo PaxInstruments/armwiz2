e6e2c734a19163e70dfc6561e3c618a6e46ba68d
Charles Pax <charles.pax@gmail.com>
2016-02-05 13:36:40 +0800

    Removed a shell test. Added a configparser test file and config files.a

---
+++ tools/configParserTest.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+import configparser
+
+def main():
+	config = configparser.ConfigParser()
+	config.read('libraries.config')
+
+	for library in config.sections():
+		print(config.get(library,'name'), end="")
+		print(': ',end="")
+		print(config.get(library,'gitURL'))
+
+if __name__ == "__main__":
+	main()
